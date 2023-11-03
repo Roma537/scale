f2da131148d093232d261848fe28b3468f925d7b
Roma <kudinovr636@gmail.com>
2023-11-03 15:21:58 +0300

    дополнение диологов(они не сделаны)

--- heroes/boris/boris.gd
+++ heroes/boris/boris.gd
@@ -1,11 +1,15 @@
 extends CharacterBody2D
 
+var enterd = false
 
 const SPEED = 300.0
 const JUMP_VELOCITY = -400.0
 
 var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
 
+func _process(delta):
+	if enterd == true:
+		pass
 
 func _physics_process(delta):
 	# Add the gravity.
@@ -17,8 +21,8 @@ func _physics_process(delta):
 
 
 func _on_area_2d_body_entered(body):
-	pass 
+	enterd = true 
 
 
 func _on_area_2d_body_exited(body):
-	pass # Replace with function body.
+	enterd = false
